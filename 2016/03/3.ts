c94eb81ffae8afa7dde2d5b93f7d33899d46f3b2
Arne Martin Aurlien <am@aurlien.net>
2021-12-07 21:28:55 +0100

    yolo

--- 2016/03/3.ts
+++ 2016/03/3.ts
@@ -3,7 +3,7 @@ import { $, chop, count, flatten, ints, lines, map, pipe, readInput, sortNumeric
 const triangles = $(readInput(), lines, map(pipe(trim, split(/\s+/), ints)))
 
 const countPossible = (triangles: number[][]): number =>
-  $(triangles, count(pipe(sortNumeric({ reverse: true }), ([s1, s2, s3]) => s1 < s2 + s3)))
+  $(triangles, count(pipe(sortNumeric(), ([s1, s2, s3]) => s3 < s1 + s2)))
 
 console.log('Part 1:', $(triangles, countPossible))
 
