1911b6e098c68c64e40a48e27d1d6e8ed9f58462
Arne Martin Aurlien <am@aurlien.net>
2021-12-08 10:56:39 +0100

    yolo

--- 2021/08/8.ts
+++ 2021/08/8.ts
@@ -24,28 +24,30 @@ import {
   without
 } from '../../common'
 
-const input = $(readInput(), lines, map(pipe(split(' | '), map(pipe(split(' '), map(split()))))))
+type C = string[][]
+
+const input: C[][] = $(readInput(), lines, map(pipe(split(' | '), map(pipe(split(' '), map(split()))))))
 
 console.log('Part 1:', $(input, map(last), flatten(), count(pipe(length, is(2, 3, 4, 7)))))
 
-const len = (n: number) => (codes: string[][]) => $(codes, filter(pipe(length, is(n))))
+const len = (n: number) => (codes: C) => $(codes, filter(pipe(length, is(n))))
 const findLen = (n: number) => pipe(len(n), first)
-const findOverlap = (code: string[], n: number) => (codes: string[][]) =>
+const findOverlap = (code: string[], n: number) => (codes: C) =>
   $(codes, filter(pipe(overlap(code), length, is(n))), first)
 
-const segment4 = (codes: string[][]) =>
+const segment4 = (codes: C) =>
   $(codes, len(5), map(without($(codes, findLen(2)))), findLen(3), overlap($(codes, findLen(4))), first)
 
-const digit0 = (codes: string[][]) => $(codes, len(6), filter(not(includes(segment4(codes)))), first)
-const digit1 = (codes: string[][]) => $(codes, findLen(2))
-const digit2 = (codes: string[][]) => $(codes, len(5), findOverlap(digit5(codes), 3))
-const digit3 = (codes: string[][]) => $(codes, len(5), findOverlap(digit1(codes), 2))
-const digit4 = (codes: string[][]) => $(codes, findLen(4))
-const digit5 = (codes: string[][]) => $(codes, len(5), findOverlap(digit6(codes), 5))
-const digit6 = (codes: string[][]) => $(codes, len(6), findOverlap(digit1(codes), 1))
-const digit7 = (codes: string[][]) => $(codes, findLen(3))
-const digit8 = (codes: string[][]) => $(codes, findLen(7))
-const digit9 = (codes: string[][]) => $(codes, len(6), filter(includes(segment4(codes))), findOverlap(digit1(codes), 2))
+const digit0 = (codes: C) => $(codes, len(6), filter(not(includes(segment4(codes)))), first)
+const digit1 = (codes: C) => $(codes, findLen(2))
+const digit2 = (codes: C) => $(codes, len(5), findOverlap(digit5(codes), 3))
+const digit3 = (codes: C) => $(codes, len(5), findOverlap(digit1(codes), 2))
+const digit4 = (codes: C) => $(codes, findLen(4))
+const digit5 = (codes: C) => $(codes, len(5), findOverlap(digit6(codes), 5))
+const digit6 = (codes: C) => $(codes, len(6), findOverlap(digit1(codes), 1))
+const digit7 = (codes: C) => $(codes, findLen(3))
+const digit8 = (codes: C) => $(codes, findLen(7))
+const digit9 = (codes: C) => $(codes, len(6), filter(includes(segment4(codes))), findOverlap(digit1(codes), 2))
 
 const digits = tee(digit0, digit1, digit2, digit3, digit4, digit5, digit6, digit7, digit8, digit9)
 
