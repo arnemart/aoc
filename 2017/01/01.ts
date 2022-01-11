864f9aa3b7937413aec5f9cab553e34cf597f82a
Arne Martin Aurlien <am@aurlien.net>
2022-01-11 16:12:36 +0100

    yolo

--- 2017/01/01.ts
+++ 2017/01/01.ts
@@ -1,23 +1,23 @@
-import { $, intdiv, ints, length, map, readInput, shift, split, sum, zipWith } from '../../common'
+import {
+  $,
+  allEqual,
+  filter,
+  first,
+  id,
+  intdiv,
+  ints,
+  length,
+  map,
+  readInput,
+  shift,
+  split,
+  sum,
+  tee,
+  zip
+} from '../../common'
 
 const digits = $(readInput(), split(), ints)
 
-console.log(
-  'Part 1:',
-  $(
-    digits,
-    zipWith($(digits, shift(1))),
-    map(([a, b]) => (a == b ? a : 0)),
-    sum
-  )
-)
+console.log('Part 1:', $(digits, tee(id, shift(1)), zip, filter(allEqual), map(first), sum))
 
-console.log(
-  'Part 1:',
-  $(
-    digits,
-    zipWith($(digits, shift($(digits, length, intdiv(2))))),
-    map(([a, b]) => (a == b ? a : 0)),
-    sum
-  )
-)
+console.log('Part 2:', $(digits, tee(id, shift($(digits, length, intdiv(2)))), zip, filter(allEqual), map(first), sum))
