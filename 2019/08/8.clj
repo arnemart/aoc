823293585727131a1182e8242e380305853e55db
Arne Martin Aurlien <am@aurlien.net>
2022-11-10 09:20:33 +0100

    yolo

---
+++ 2019/08/8.clj
@@ -0,0 +1,29 @@
+(ns aoc.2019.08.8
+  (:require [aoc.common :refer [count-where pick read-input zip]]
+            [clojure.string :as str]))
+
+(def w 25)
+(def h 6)
+
+(defn -main []
+  (let [layers (->> (read-input :split-with #"")
+                    (mapv parse-long)
+                    (partition (* w h)))]
+
+    (->> layers
+         (sort-by #(count-where zero? %))
+         first
+         frequencies
+         (pick [1 2])
+         (apply *)
+         (println "Part 1:"))
+
+    (println "Part 2:")
+    (->> layers
+         zip
+         (map (fn [px] (first (filter #(<= % 1) px))))
+         (map #(get {0 " " 1 "#"} %))
+         (partition w)
+         (map #(str/join "" %))
+         (str/join "\n")
+         println)))
--- common.clj
+++ common.clj
@@ -15,6 +15,15 @@
          slurp))
    (str/split split-with)))
 
+(defn count-where [fn coll]
+  (count (filter fn coll)))
+
+(defn pick [keys coll]
+  (map #(get coll %) keys))
+
+(defn zip [lists]
+  (apply (partial mapv vector) lists))
+
 (defn parse [s] (edn/read-string s))
 
 (defn spy [v] (pprint v) v)
