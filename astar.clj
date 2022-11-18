0619ec28b24e09be82e43a540eff70d7fe49ecc5
Arne Martin Aurlien <am@aurlien.net>
2022-11-18 13:10:54 +0100

    yolo

--- astar.clj
+++ astar.clj
@@ -1,7 +1,7 @@
 (ns aoc.astar
   (:require [clojure.data.priority-map :refer [priority-map]]))
 
-(defn reconstruct-path 
+(defn reconstruct-path
   ([node] (reconstruct-path node []))
   ([node path]
    (if (nil? (:parent node))
@@ -26,32 +26,32 @@
       ;; Done!
       (is-end (:data node)) {:cost (:g node) :path (reconstruct-path node)}
       ;; Not done yet
-      :else (let [closed-set (conj closed-set (:data node))
-                  open-map (dissoc open-map (:data node))
-                  [priorities open-map]
-                  (->> (get-neighbors (:data node))
-                       (filter #(not (contains? closed-set %)))
-                       (reduce
-                        (fn [[priorities open-map] neighbor-data]
-                          (let [g-from-this-node (+ (:g node) (calculate-cost (:data node) neighbor-data))
-                                new-neighbor-node {:data neighbor-data
-                                                   :parent node
-                                                   :g g-from-this-node
-                                                   :h (heuristic neighbor-data)}]
-                            (if (contains? open-map neighbor-data)
-                              ;; We have seen this node before
-                              (let [neighbor-node (get open-map neighbor-data)]
-                                (if (< (:g neighbor-node) g-from-this-node)
-                                  ;; Skip it, another route is faster
-                                  [priorities open-map]
-                                  ;; Found a better route, update existing neighbor with this node as its new parent
-                                  [(-> priorities
-                                       (dissoc neighbor-node)
-                                       (assoc new-neighbor-node (+ g-from-this-node (:h new-neighbor-node))))
-                                   (assoc open-map neighbor-data new-neighbor-node)]))
-                              ;; Add new neighbor to open nodes
-                              [(assoc priorities new-neighbor-node (+ g-from-this-node (:h new-neighbor-node)))
-                               (assoc open-map neighbor-data new-neighbor-node)])))
-                        [priorities open-map]))]
+      :else
+      (let [closed-set (conj closed-set (:data node))
+            open-map (dissoc open-map (:data node))
+            [priorities open-map]
+            (->> (get-neighbors (:data node))
+                 (filter #(not (contains? closed-set %)))
+                 (reduce
+                  (fn [[priorities open-map] neighbor-data]
+                    (let [g-from-this-node (+ (:g node) (calculate-cost (:data node) neighbor-data))]
+                      (if (and (contains? open-map neighbor-data)
+                               (< (get-in open-map [neighbor-data :g]) g-from-this-node))
+                        ;; We have seen this node before and already have a faster path to it
+                        [priorities open-map]
+                        (let [new-neighbor-node {:data neighbor-data
+                                                 :parent node
+                                                 :g g-from-this-node
+                                                 :h (heuristic neighbor-data)}]
+                          (if-let [old-neighbor-node (get open-map neighbor-data)]
+                            ;; Found a better route, update neighbor with this node as its new parent
+                            [(-> priorities
+                                 (dissoc old-neighbor-node)
+                                 (assoc new-neighbor-node (+ g-from-this-node (:h new-neighbor-node))))
+                             (assoc open-map neighbor-data new-neighbor-node)]
+                            ;; Add new neighbor to open nodes
+                            [(assoc priorities new-neighbor-node (+ g-from-this-node (:h new-neighbor-node)))
+                             (assoc open-map neighbor-data new-neighbor-node)])))))
+                  [priorities open-map]))]
 
-              (recur closed-set open-map (pop priorities) (first (peek priorities)))))))
+        (recur closed-set open-map (pop priorities) (first (peek priorities)))))))
