5f497fa80f46e0037f117a81832d36afc611ba1f
Arne Martin Aurlien <am@aurlien.net>
2022-12-02 15:55:11 +0100

    yolo

--- 2022/02/2.stupid.js
+++ 2022/02/2.stupid.js
@@ -1,3 +1,3 @@
 i=(require('fs').readFileSync('input.txt')+'').replace(/ /g,'').split('\n')
-console.log(i.reduce((a,b)=>a+{AX:4,AY:8,AZ:3,BX:1,BY:5,BZ:9,CX:7,CY:2,CZ:6}[b],0),
-i.reduce((a,b)=>a+{AX:3,AY:4,AZ:8,BX:1,BY:5,BZ:9,CX:2,CY:6,CZ:7}[b],0))
+console.log(i.reduce((a,b)=>a+'  BXCYAZAXBYCZCXAYBZ'.indexOf(b)/2,0),
+i.reduce((a,b)=>a+'  BXCXAXAYBYCYCZAZBZ'.indexOf(b)/2,0))
