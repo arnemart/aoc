5847dd9a23a90a05fc44deb90e8374bc925dfc25
Arne Martin Aurlien <am@aurlien.net>
2022-12-03 12:02:56 +0100

    yolo

--- 2022/03/3.py
+++ 2022/03/3.py
@@ -3,12 +3,12 @@ def priority(s):
   return i - (96 if (i > 96) else 38)
 
 def overlaps(sets):
-  return [list(s.intersection(*ss))[0] for [s, *ss] in sets]
+  return [s.intersection(*ss).pop() for [s, *ss] in sets]
 
 backpacks = [list(line.strip()) for line in open("input.txt")]
 
 compartments = [[set(bp[0:len(bp) // 2]), set(bp[len(bp) // 2:])] for bp in backpacks]
 print("Part 1:", sum(priority(x) for x in overlaps(compartments)))
 
-groups = [map(set, backpacks[i * 3:i * 3 + 3]) for i in range(len(backpacks) // 3)]
+groups = [map(set, backpacks[i:i + 3]) for i in range(0, len(backpacks), 3)]
 print("Part 2:", sum(priority(x) for x in overlaps(groups)))
