52b109d8da2e4bfba2a9605bbeab38e762986fb5
Arne Martin Aurlien <am@aurlien.net>
2022-12-04 18:05:13 +0100

    yolo

--- 2022/04/4.stupid.py
+++ 2022/04/4.stupid.py
@@ -1,2 +1,2 @@
 import re
-print(*map(sum,zip(*[[a<=c<=d<=b or c<=a<=b<=d,b>=c and d>=a]for[a,b,c,d]in[map(int,re.findall("\d+",l))for l in open("input.txt")]])))
+print(*map(sum,zip(*[[a<=c<=d<=b or c<=a<=b<=d,b>=c and d>=a]for[a,b,c,d]in[map(int,re.split("\D",l,3))for l in open("input.txt")]])))
