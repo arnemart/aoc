46c06a3905258c062671b599fbe50b1ce0d8fef0
Arne Martin Aurlien <am@aurlien.net>
2022-12-05 15:57:28 +0100

    yolo

--- 2022/03/3.stupid.py
+++ 2022/03/3.stupid.py
@@ -1,3 +1,3 @@
 p=lambda s:ord(s)-(96,38)[ord(s)<96]
 r=open("input.txt").read().split()
-print(sum(p((set(l[len(l)//2:])&set(l[:len(l)//2])).pop()) for l in r),sum(p((set(r[i])&set(r[i+1])&set(r[i+2])).pop()) for i in range(0,len(r),3)))
+print(sum(p((set(l[len(l)//2:])&set(l[:len(l)//2])).pop())for l in r),sum(p((set(r[i])&set(r[i+1])&set(r[i+2])).pop())for i in range(0,len(r),3)))
