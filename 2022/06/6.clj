8bd21ae6ed60091b78a6017219408b16b232ae57
Arne Martin Aurlien <am@aurlien.net>
2022-12-06 13:29:04 +0100

    yolo

--- 2022/06/6.clj
+++ 2022/06/6.clj
@@ -1,15 +1,12 @@
 (ns aoc.2022.06.6
-  (:require [aoc.common :refer [read-input zip]]))
+  (:require [aoc.common :refer [read-input]]))
 
 (defn find-marker [signal distinct]
-  (->> (range distinct)
-       (map #(drop % signal))
-       (apply zip)
-       (map set)
-       (keep-indexed #(when (= (count %2) distinct) %1))
+  (->> (range (count signal))
+       (filter #(-> signal (subvec % (+ % distinct)) set count (= distinct)))
        first
        (+ distinct)))
 
-(let [signal (read-input :split-with #"")]
-  (println "Part 1:" (find-marker signal 4))
-  (println "Part 2:" (find-marker signal 14)))
+(time (let [signal (read-input :split-with #"")]
+        (println "Part 1:" (find-marker signal 4))
+        (println "Part 2:" (find-marker signal 14))))
