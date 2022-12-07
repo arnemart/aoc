656e40f533dd3f6eea7113a4969a316162726612
Arne Martin Aurlien <am@aurlien.net>
2022-12-07 22:18:46 +0100

    yolo

--- 2022/07/7.clj
+++ 2022/07/7.clj
@@ -2,33 +2,21 @@
   (:require [aoc.common :refer [read-input sum]]
             [clojure.string :as str]))
 
-(defn build-tree
-  ([cmds] (build-tree cmds {} []))
-  ([cmds tree path]
+(defn build-folders
+  ([cmds] (build-folders cmds {} []))
+  ([cmds folders path]
    (if-let [[cmd & cmds] cmds]
      (let [parts (str/split cmd #" ")]
        (condp re-matches cmd
-         #"\$ cd \.\." (recur cmds tree (pop path))
-         #"\$ cd .+"   (recur cmds tree (conj path (last parts)))
-         #"\d+ .+"     (recur cmds (assoc-in tree (conj path :files (last parts)) (parse-long (first parts))) path)
-         (recur cmds tree path)))
-     tree)))
+         #"\$ cd \.\." (recur cmds folders (pop path))
+         #"\$ cd .+"   (recur cmds folders (conj path (last parts)))
+         #"\d+ .+"     (recur cmds (->> (range (count path))
+                                        (map #(take (inc %) path))
+                                        (reduce #(assoc %1 %2 (+ (get %1 %2 0) (parse-long (first parts)))) folders)) path)
+         (recur cmds folders path)))
+     folders)))
 
-(defn folder-sizes
-  ([tree] (folder-sizes tree {} []))
-  ([tree folders path]
-   (->> (get-in tree path)
-        keys
-        (filter #(not= :files %))
-        (reduce (fn [folders f]
-                  (let [p (conj path f)
-                        folders (folder-sizes tree folders p)
-                        filesize (-> (get-in tree (conj p :files)) vals sum)
-                        subfoldersize (->> folders (filter #(= p (pop (first %)))) vals sum)]
-                    (assoc folders p (+ filesize subfoldersize))))
-                folders))))
-
-(let [folders (-> (read-input) build-tree folder-sizes)
+(let [folders (-> (read-input) build-folders)
       space-needed (- 30000000 (- 70000000 (get folders ["/"])))]
 
   (->> folders
