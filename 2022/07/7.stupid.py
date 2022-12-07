cb018863d83e33adf6e539a0d80313064ceef155
Arne Martin Aurlien <am@aurlien.net>
2022-12-07 21:59:24 +0100

    yolo

--- 2022/07/7.stupid.py
+++ 2022/07/7.stupid.py
@@ -3,8 +3,6 @@ p=()
 for c in open("input.txt"):
  d,e,*f=c.split()
  if e=="cd":p=p[:-1]if c[5]=="."else(*p,*f)
- if c[0].isdigit():
-  s=int(d)
-  for i in range(len(p)):g[p[:i+1]]=s+g.get(p[:i+1],0)
-v=g.values()
-print(sum(f for f in v if f<1e5),min(f for f in v if f>g[("/",)]-4e7))
+ if d.isdigit():
+  for i in range(len(p)):g[p[:i+1]]=int(d)+g.get(p[:i+1],0)
+print(sum(g[f]for f in g if g[f]<1e5),min(g[f]for f in g if g[f]>g[("/",)]-4e7))
