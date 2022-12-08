79db4c8d9e77bce80cc2deadbd89c33c4a3c6a71
Arne Martin Aurlien <am@aurlien.net>
2022-12-08 19:58:34 +0100

    yolo

--- 2022/08/8.stupid.py
+++ 2022/08/8.stupid.py
@@ -1,10 +1,10 @@
-q=range
-f=[[int(t)for t in l]for l in open("input.txt").read().split()]
-h=len(f)
+q,l=range,len
+f=[[int(t)for t in l.strip()]for l in open("input.txt")]
+h=l(f)
 g=lambda y,x:[[f[k][x]for k in q(y)][::-1],[f[y][l]for l in q(x)][::-1],[f[k][x]for k in q(y+1,h)],[f[y][l]for l in q(x+1,h)]]
-def v(t,g):
- h=[i for i in q(len(t))if t[i]>=g]
- return len(t[:h[0]+1]if len(h)>0 else t)
+def v(t,z):
+ h=[i for i in q(l(t))if t[i]>=z]
+ return l(l(h)>0 and t[:h[0]+1]or t)
 def s(y,x):
  u,l,d,r=[v(t,f[y][x])for t in g(y,x)]
  return u*l*d*r
