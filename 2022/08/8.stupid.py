9cf0f11ce2e0b6987e21be3d6dfd108689a7837d
Arne Martin Aurlien <am@aurlien.net>
2022-12-08 21:48:49 +0100

    yolo

--- 2022/08/8.stupid.py
+++ 2022/08/8.stupid.py
@@ -1,5 +1,5 @@
 q,l=range,len
-f=[[int(t)for t in l.strip()]for l in open("input.txt")]
+f=[[int(t)for t in l.strip()]for l in open("a")]
 h=l(f)
 g=lambda y,x:[[f[k][x]for k in q(y)][::-1],[f[y][l]for l in q(x)][::-1],[f[k][x]for k in q(y+1,h)],[f[y][l]for l in q(x+1,h)]]
 def v(t,z):
@@ -9,4 +9,4 @@ def s(y,x):
  u,l,d,r=[v(t,f[y][x])for t in g(y,x)]
  return u*l*d*r
 r=[[a,b]for a in q(h)for b in q(h)]
-print(sum(1 for a in r if 0 in a or h-1 in a or min(max(t)for t in g(*a))<f[a[0]][a[1]]),max(s(*a)for a in r))
+print(sum(0 in a or h-1 in a or min(max(t)for t in g(*a))<f[a[0]][a[1]]for a in r),max(s(*a)for a in r))
