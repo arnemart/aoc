f3c59c1e286f1ff9ec10a5be2a1c5959c86893d5
Arne Martin Aurlien <am@aurlien.net>
2022-12-09 10:51:48 +0100

    yolo

--- 2022/08/8.stupid.py
+++ 2022/08/8.stupid.py
@@ -4,7 +4,7 @@ h=l(f)
 g=lambda y,x:[[f[k][x]for k in q(y)][::-1],[f[y][l]for l in q(x)][::-1],[f[k][x]for k in q(y+1,h)],[f[y][l]for l in q(x+1,h)]]
 def v(t,z):
  h=[i for i in q(l(t))if t[i]>=z]
- return l(l(h)>0 and t[:h[0]+1]or t)
+ return l(l(h)and t[:h[0]+1]or t)
 def s(y,x):
  u,l,d,r=[v(t,f[y][x])for t in g(y,x)]
  return u*l*d*r
