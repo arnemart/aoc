aadd872ccdfd6537e6ca84b8ce5b9c96211108e4
Arne Martin Aurlien <am@aurlien.net>
2022-12-12 14:22:53 +0100

    yolo

--- 2022/12/12.clj
+++ 2022/12/12.clj
@@ -1,15 +1,22 @@
 (ns aoc.2022.12.12
   (:require [aoc.astar :refer [astar]]
-            [aoc.common :refer [find-index manhattan read-input]]
-            [clojure.math.combinatorics :as combo]))
+            [aoc.common :refer [find-index read-input]]))
 
-(defn neighbors [hm [y x]]
-  (->> [[(dec y) x]
-        [(inc y) x]
-        [y (dec x)]
-        [y (inc x)]]
+(defn adjacent [[y x]]
+  [[(dec y) x]
+   [(inc y) x]
+   [y (dec x)]
+   [y (inc x)]])
+
+(defn neighbors-1 [hm p]
+  (->> (adjacent p)
        (filter #(when-let [v (get-in hm %)]
-                  (<= v (inc (get-in hm [y x])))))))
+                  (<= v (inc (get-in hm p)))))))
+
+(defn neighbors-2 [hm p]
+  (->> (adjacent p)
+       (filter #(when-let [v (get-in hm %)]
+                  (<= (dec (get-in hm p)) v)))))
 
 (let [input (read-input)
       start-y (->> input (find-index #(re-matches #".*S.*" %2)))
@@ -17,28 +24,21 @@
       start-x (->> (nth input start-y) (find-index #(= \S %2)))
       end-x   (->> (nth input end-y) (find-index #(= \E %2)))
       start [start-y start-x]
+      end [end-y end-x]
       heightmap (-> (mapv #(mapv int %) input)
                     (assoc-in [start-y start-x] (int \a))
-                    (assoc-in [end-y end-x] (int \z)))
-      all-low-points (->> (combo/cartesian-product (range (count heightmap)) (range (count (first heightmap))))
-                          (filter #(= (int \a) (get-in heightmap %))))
-      is-end (partial = [end-y end-x])
-      heuristic (partial manhattan [end-y end-x])]
+                    (assoc-in [end-y end-x] (int \z)))]
 
   (->> (astar :start start
-              :is-end is-end
-              :get-neighbors (partial neighbors heightmap)
-              :heuristic heuristic)
+              :is-end (partial = end)
+              :get-neighbors (partial neighbors-1 heightmap)
+              :heuristic #(- (int \z) (get-in heightmap %)))
        :cost
        (println "Part 1:"))
 
-  (->> (astar :start start
-              :is-end is-end
-              :get-neighbors #(let [n (neighbors heightmap %)]
-                                (if (= start %)
-                                  (apply conj all-low-points n)
-                                  n))
-              :calculate-cost #(if (= (int \a) (get-in heightmap %2)) 0 1)
-              :heuristic heuristic)
+  (->> (astar :start [end-y end-x]
+              :is-end #(= (int \a) (get-in heightmap %))
+              :get-neighbors (partial neighbors-2 heightmap)
+              :heuristic #(- (get-in heightmap %) (int \a)))
        :cost
        (println "Part 2:")))
