d5fdbc40da7cec1c65d5168acf70bc33bc3103f3
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-01 15:04:42 +0100

    yolo

--- 2023/01/1.py
+++ 2023/01/1.py
@@ -1,5 +1,4 @@
 import re
-d=["_","one","two","three","four","five","six","seven","eight","nine"]
-s=open("input.txt").read().split("\n")
-i=lambda a:str(d.index(a))if len(a)>1 else a
-print(sum(int(r[0]+r[-1])for r in[re.findall("\d",l)for l in s]),sum(int(i(a[0])+i(a[-1]))for a in[re.findall("(?=(\d|"+'|'.join(d)+"))",l) for l in s]))
+z=s=open("input.txt").read()
+for i,j in enumerate(["_","one","two","three","four","five","six","seven","eight","nine"]):z=re.sub(j,lambda m:m[0]+str(i)+m[0],z)
+print([sum(int(r[0]+r[-1])for r in re.sub("[a-z]","",g).split("\n"))for g in[s,z]])
