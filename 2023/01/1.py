12b6775e5cdb1648c37d94570aa1005fba1712cc
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-01 16:38:47 +0100

    yolo

--- 2023/01/1.py
+++ 2023/01/1.py
@@ -1,4 +1,4 @@
 import re
 z=s=open("a").read()
-for i,j in enumerate(["on","tw","hre","ou","fi","si","sev","ei","ni"],1):z=re.sub(j,lambda m:str(i)+m[0],z)
+for i,j in enumerate(["on","tw","hre","ou","fi","si","sev","ei","ni"],1):z=z.replace(j,str(i)+j)
 print([sum(int(r[0]+r[-1])for r in re.sub("[a-z]","",g).split("\n"))for g in[s,z]])
