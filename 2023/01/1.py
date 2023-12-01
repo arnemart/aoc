6b70b1453d3550096a6c0c811f07043aa07001b2
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-01 15:07:05 +0100

    yolo

--- 2023/01/1.py
+++ 2023/01/1.py
@@ -1,4 +1,4 @@
 import re
 z=s=open("input.txt").read()
-for i,j in enumerate(["_","one","two","three","four","five","six","seven","eight","nine"]):z=re.sub(j,lambda m:m[0]+str(i)+m[0],z)
+for i,j in enumerate(["one","two","three","four","five","six","seven","eight","nine"],1):z=re.sub(j,lambda m:m[0]+str(i)+m[0],z)
 print([sum(int(r[0]+r[-1])for r in re.sub("[a-z]","",g).split("\n"))for g in[s,z]])
