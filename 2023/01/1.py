b9929812b38461fc0a5b7ed0312882ea9ebd7ac5
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-01 18:28:16 +0100

    yolo

--- 2023/01/1.py
+++ 2023/01/1.py
@@ -1,3 +1,3 @@
 z=s=open("a").read()
-for i,j in enumerate(["on","tw","hre","ou","fi","si","sev","ei","ni"],1):z=z.replace(j,str(i)+j)
+for i,j in enumerate("on tw hre ou fi si sev ei ni".split(),1):z=z.replace(j,str(i)+j)
 print([sum(int(r[0]+r[-1])for r in"".join(c for c in g if c<"a").split("\n"))for g in[s,z]])
