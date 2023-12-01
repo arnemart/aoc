1929f667796494d716e037738c8888899093388b
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-01 16:45:18 +0100

    yolo

--- 2023/01/1.py
+++ 2023/01/1.py
@@ -1,4 +1,3 @@
-import re
 z=s=open("a").read()
 for i,j in enumerate(["on","tw","hre","ou","fi","si","sev","ei","ni"],1):z=z.replace(j,str(i)+j)
-print([sum(int(r[0]+r[-1])for r in re.sub("[a-z]","",g).split("\n"))for g in[s,z]])
+print([sum(int(r[0]+r[-1])for r in"".join(c for c in g if c<"a").split("\n"))for g in[s,z]])
