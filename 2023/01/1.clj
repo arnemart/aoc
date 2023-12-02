b304ded5b130e4d71e167cfe562a8ad84d7d91bd
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-02 11:51:20 +0100

    yolo

--- 2023/01/1.clj
+++ 2023/01/1.clj
@@ -11,9 +11,7 @@
        (apply +)))
 
 (let [input (read-input)]
-  (->> input
-       calibrate
-       (println "Part 1:"))
+  (println "Part 1:" (calibrate input))
   (->> input
        (map (fn [s]
               (reduce-kv #(str/replace %1 %3 (str %3 %2 %3)) s digits)))
