677e3e81f938c964f83c01ff88ac248ed8d7112a
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-04 08:40:57 +0100

    yolo

---
+++ 2023/04/4.clj
@@ -0,0 +1,37 @@
+(ns aoc.2023.04.4 
+  (:require [aoc.common :refer [read-input]]
+            [clojure.math.numeric-tower :refer [expt]]
+            [clojure.set :as set]
+            [clojure.string :as str]))
+
+(let [wins (->> (read-input)
+                (map #(str/replace % #".*:" ""))
+                (map #(str/split % #"\|"))
+                (mapv (fn [line]
+                        (->> line
+                             (map #(re-seq #"\d+" %))
+                             (map #(map parse-long %))
+                             (map set))))
+                (map #(apply set/intersection %))
+                (mapv count))
+
+      won-cards-initial (->> (range (count wins))
+                             (map #(vector % 1))
+                             (into {}))
+      
+      won-cards (->> (range (count wins))
+                     (reduce (fn [cards card]
+                               (->> (range (inc card) (+ card (get wins card) 1))
+                                    (reduce (fn [cs c] (update cs c #(+ % (get cards card)))) cards)))
+                             won-cards-initial))]
+
+  (->> wins
+       (filter #(> % 0))
+       (map #(expt 2 (dec %)))
+       (apply +)
+       (println "Part 1:"))
+
+  (->> won-cards
+       vals
+       (apply +)
+       (println "Part 2:")))
