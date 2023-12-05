8b3c10a1448d0f7c214c8c7837a3076849b485a3
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-05 14:45:32 +0100

    yolo

--- 2023/05/5.clj
+++ 2023/05/5.clj
@@ -3,9 +3,9 @@
             [clojure.string :as str]))
 
 (defn convert [n ms]
-  (reduce (fn [n [dest source len]]
-            (if (<= source n (+ source len -1))
-              (reduced (+ n (- dest source)))
+  (reduce (fn [n [from to dest]]
+            (if (<= from n to)
+              (reduced (+ n (- dest from)))
               n)) n ms))
 
 (defn in-ranges [n ranges]
@@ -13,22 +13,25 @@
        (some (fn [[a b]] (<= a n b)))))
 
 (let [[seeds maps] (->> (read-input :split-with #"\n\n")
-                        (tee [#(split-to-ints (first %))
-                              #(->> (drop 1 %)
+                        (tee [#(->> % first split-to-ints)
+                              #(->> % rest
                                     (map (fn [m]
                                            (->> (str/split-lines m)
                                                 (drop 1)
                                                 (map split-to-ints)))))]))
+      forward-maps (->> maps
+                        (map #(map (fn [[dest source len]]
+                                     [source (+ source len -1) dest]) %)))
       reverse-maps (->> maps
                         reverse
-                        (map #(map (fn [[dest source len]]
-                                     [source dest len]) %)))
+                        (map #(map (fn [[source dest len]]
+                                     [source (+ source len -1) dest]) %))) 
       seed-ranges (->> seeds
                        (partition 2)
-                       (map (fn [[a b]] (list a (+ a b -1)))))]
+                       (map (fn [[a b]] [a (+ a b -1)])))]
 
   (->> seeds
-       (map #(reduce convert % maps))
+       (map #(reduce convert % forward-maps))
        (apply min)
        (println "Part 1:"))
 
