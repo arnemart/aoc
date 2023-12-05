317fbb015dbcc891e72ee388af0957464bea91f8
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-05 13:32:12 +0100

    yolo

--- 2023/05/5.clj
+++ 2023/05/5.clj
@@ -2,21 +2,18 @@
   (:require [aoc.common :refer [read-input split-to-ints tee]]
             [clojure.string :as str]))
 
-(defn convert-single [n [dest source len]]
-  (if (<= source n (+ source (dec len)))
-    (+ n (- dest source))
-    n))
-
 (defn convert [n ms]
-  (reduce #(let [newn (convert-single %1 %2)]
-             (if (= n newn)
-               n
-               (reduced newn))) n ms))
+  (reduce (fn [n [dest source len]]
+            (let [newn (if (<= source n (+ source len -1))
+                         (+ n (- dest source))
+                         n)]
+              (if (= n newn)
+                n
+                (reduced newn)))) n ms))
 
-(defn in-seeds [n seeds]
-  (->> seeds
-       (partition 2)
-       (some (fn [[a b]] (<= a n (+ a (dec b)))))))
+(defn in-ranges [n ranges]
+  (->> ranges
+       (some (fn [[a b]] (<= a n b)))))
 
 (let [[seeds maps] (->> (read-input :split-with #"\n\n")
                         (tee [#(split-to-ints (first %))
@@ -28,7 +25,10 @@
       reverse-maps (->> maps
                         reverse
                         (map #(map (fn [[dest source len]]
-                                     [source dest len]) %)))]
+                                     [source dest len]) %)))
+      seed-ranges (->> seeds
+                       (partition 2)
+                       (map (fn [[a b]] (list a (+ a b -1)))))]
 
   (->> seeds
        (map #(reduce convert % maps))
@@ -36,6 +36,6 @@
        (println "Part 1:"))
 
   (->> (range)
-       (filter #(in-seeds (reduce convert % reverse-maps) seeds))
+       (filter #(in-ranges (reduce convert % reverse-maps) seed-ranges))
        first
        (println "Part 2:")))
