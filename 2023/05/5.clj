2175f232db46bb06f886db654315b4eadbfac681
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-05 22:29:49 +0100

    yolo

--- 2023/05/5.clj
+++ 2023/05/5.clj
@@ -3,40 +3,54 @@
             [clojure.string :as str]))
 
 (defn convert [n ms]
-  (reduce (fn [n [from to dest]]
-            (if (<= from n to)
-              (reduced (+ n (- dest from)))
-              n)) n ms))
+  (or
+   (some (fn [[from to dest]]
+           (when (<= from n to)
+             (+ n (- dest from)))) ms)
+   n))
 
-(defn in-ranges [n ranges]
-  (->> ranges
-       (some (fn [[a b]] (<= a n b)))))
+(defn convert-slice [[ss se] [from to dest]]
+  (cond
+    (or (<= ss se from to) (<= from to ss se)) [[] [[ss se]]]
+    (<= from ss se to) [[(+ ss (- dest from)) (+ se (- dest from))] []]
+    (<= ss from se to) [[dest (+ se (- dest from))] [[ss from]]]
+    (<= from ss to se) [[(+ ss (- dest from)) (+ to (- dest from))] [[to se]]]
+    (<= ss from to se) [[dest (+ to (- dest from))] [[ss from] [to se]]]))
 
-(let [[seeds maps] (->> (read-input :split-with #"\n\n")
-                        (tee [#(->> % first split-to-ints)
-                              #(->> % rest
-                                    (map (fn [m]
-                                           (->> (str/split-lines m)
-                                                (drop 1)
-                                                (map split-to-ints)))))]))
-      forward-maps (->> maps
-                        (map #(map (fn [[dest source len]]
-                                     [source (+ source len -1) dest]) %)))
-      reverse-maps (->> maps
-                        reverse
-                        (map #(map (fn [[source dest len]]
-                                     [source (+ source len -1) dest]) %)))
+(defn convert-maps [s ms]
+  (loop [done [] pending s [m & ms] ms]
+    (let [[mapped leftover]
+          (reduce (fn [[mapped leftover] p]
+                    (let [[m l] (convert-slice p m)]
+                      [(if (empty? m) mapped (conj mapped m)) (concat leftover l)]))
+                  [[] []] pending)
+          done (concat done mapped)]
+      (if (empty? ms)
+        (concat done leftover)
+        (recur done leftover ms)))))
+
+(let [[seeds maps]
+      (->> (read-input :split-with #"\n\n")
+           (tee [#(->> % first split-to-ints)
+                 #(->> % rest
+                       (map (fn [m]
+                              (->> (str/split-lines m)
+                                   (drop 1)
+                                   (map split-to-ints)
+                                   (map (fn [[dest source len]]
+                                          [source (+ source len -1) dest]))))))]))
       seed-ranges (->> seeds
                        (partition 2)
                        (map (fn [[from len]]
-                              [from (+ from len -1)])))]
+                              [from (+ from len)])))]
 
   (->> seeds
-       (map #(reduce convert % forward-maps))
+       (map #(reduce convert % maps))
        (apply min)
        (println "Part 1:"))
 
-  (->> (range)
-       (filter #(in-ranges (reduce convert % reverse-maps) seed-ranges))
-       first
+  (->> maps
+       (reduce convert-maps seed-ranges)
+       (map first)
+       (apply min)
        (println "Part 2:")))
