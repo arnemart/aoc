d48f39bea402102e4a4ca803b14d7917d0ec3a9c
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-08 09:35:50 +0100

    yolo

---
+++ 2023/08/8.clj
@@ -0,0 +1,28 @@
+(ns aoc.2023.08.8
+  (:require [aoc.common :refer [read-input]]
+            [clojure.math.numeric-tower :refer [lcm]]
+            [clojure.string :as str]))
+
+(defn solve [start test dirs nodes]
+  (loop [cur start [d & ds] dirs steps 0]
+    (if (test cur) steps
+        (recur (get-in nodes [cur d]) ds (inc steps)))))
+
+(let [[dirs-str nodes-str] (read-input :split-with #"\n\n")
+      dirs (->> (str/split dirs-str #"")
+                (map #(if (= "L" %) 0 1))
+                cycle)
+      nodes (->> (str/split nodes-str #"\n")
+                 (map #(re-find #"^(\w+) = \((\w+), (\w+)\)$" %))
+                 (map (fn [[_, a, b, c]] [a [b c]]))
+                 (into {}))
+      ends-with-z #(= \Z (last %))]
+
+  (println "Part 1:" (solve "AAA" #(= "ZZZ" %) dirs nodes))
+
+  (->> nodes
+       keys
+       (filter #(str/ends-with? % "A"))
+       (map #(solve % ends-with-z dirs nodes))
+       (reduce lcm)
+       (println "Part 2:")))
