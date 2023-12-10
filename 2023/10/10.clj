0e15a761f91f6b86765043b40c3d54d609a96bf6
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-10 23:18:18 +0100

    yolo

---
+++ 2023/10/10.clj
@@ -0,0 +1,58 @@
+(ns aoc.2023.10.10 
+  (:require [aoc.common :refer [read-input]]
+            [clojure.math.combinatorics :as combo]
+            [clojure.string :as str]))
+
+(defn find-next [pipes [y x] [py px]]
+  (case (get-in pipes [y x])
+    "|" (if (> y py) [(inc y) x] [(dec y) x])
+    "-" (if (> x px) [y (inc x)] [y (dec x)])
+    "L" (if (= x px) [y (inc x)] [(dec y) x])
+    "J" (if (= x px) [y (dec x)] [(dec y) x])
+    "7" (if (= x px) [y (dec x)] [(inc y) x])
+    "F" (if (= x px) [y (inc x)] [(inc y) x])
+    "S" nil))
+
+(defn get-path [pipes start prev]
+  (loop [path [start] cur start prev prev]
+    (if-let [n (find-next pipes cur prev)]
+      (recur (conj path n) n cur)
+      path)))
+
+(defn enclosed? [pipes path [y x]]
+  (let [str-to-the-left (->> path
+                             (filter (fn [[py px]] (and (= y py) (< px x))))
+                             sort
+                             (map #(get-in pipes %))
+                             str/join)
+        massaged-str (-> str-to-the-left
+                         (str/replace #"L-*J" "")
+                         (str/replace #"F-*7" "")
+                         (str/replace #"F-*J" "|")
+                         (str/replace #"L-*7" "|"))]
+    (->> massaged-str
+         (filter #(= \| %))
+         count
+         odd?)))
+
+(let [pipes (->> (read-input)
+                 (mapv #(str/split % #"")))
+      [[sy sx]] (->> pipes
+                     (keep-indexed (fn [y l]
+                                     (let [[x] (keep-indexed #(when (= "S" %2) %1) l)]
+                                       (when (some? x) [y x])))))
+      [ny nx] [(inc sy) sx]
+      path (get-path pipes [ny nx] [sy sx])
+      path-set (set path)
+      outside-path (->> (set (combo/cartesian-product
+                              (range (count pipes))
+                              (range (count (first pipes)))))
+                        (filter #(not (contains? path-set %))))
+      pipes-without-s (assoc-in pipes [sy sx] "|")]
+
+  (println "Part 1:" (/ (count path) 2))
+
+  (->> outside-path
+       (filter (partial enclosed? pipes-without-s path))
+       count
+       (println "Part 2:")))
