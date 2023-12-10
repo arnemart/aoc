136441d41f0a28f6d16c2a35812085f4b710d3f2
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-10 23:31:53 +0100

    yolo

--- 2023/10/10.clj
+++ 2023/10/10.clj
@@ -10,14 +10,14 @@
     "L" (if (= x px) [y (inc x)] [(dec y) x])
     "J" (if (= x px) [y (dec x)] [(dec y) x])
     "7" (if (= x px) [y (dec x)] [(inc y) x])
-    "F" (if (= x px) [y (inc x)] [(inc y) x])
-    "S" nil))
+    "F" (if (= x px) [y (inc x)] [(inc y) x])))
 
 (defn get-path [pipes start prev]
   (loop [path [start] cur start prev prev]
-    (if-let [n (find-next pipes cur prev)]
-      (recur (conj path n) n cur)
-      path)))
+    (let [n (find-next pipes cur prev)]
+      (if (= start n)
+        path
+        (recur (conj path n) n cur)))))
 
 (defn enclosed? [pipes path [y x]]
   (let [str-to-the-left (->> path
@@ -41,18 +41,18 @@
                      (keep-indexed (fn [y l]
                                      (let [[x] (keep-indexed #(when (= "S" %2) %1) l)]
                                        (when (some? x) [y x])))))
-      [ny nx] [(inc sy) sx]
+      [ny nx] [(dec sy) sx]
+      pipes (assoc-in pipes [sy sx] "|")
       path (get-path pipes [ny nx] [sy sx])
       path-set (set path)
       outside-path (->> (set (combo/cartesian-product
                               (range (count pipes))
                               (range (count (first pipes)))))
-                        (filter #(not (contains? path-set %))))
-      pipes-without-s (assoc-in pipes [sy sx] "|")]
+                        (filter #(not (contains? path-set %))))]
 
   (println "Part 1:" (/ (count path) 2))
 
   (->> outside-path
-       (filter (partial enclosed? pipes-without-s path))
+       (filter (partial enclosed? pipes path))
        count
        (println "Part 2:")))
