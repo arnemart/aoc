82176f5f3ce1b4923141fdf0ee914906e002f397
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-11 12:29:45 +0100

    yolo

--- 2023/10/10.clj
+++ 2023/10/10.clj
@@ -30,36 +30,35 @@
          (re-seq #"\||F-*J|L-*7")
          count
          odd?)))
-(time
- (let [pipes (->> (read-input)
-                  (mapv #(str/split % #"")))
-       [[sy sx]] (->> pipes
-                      (keep-indexed (fn [y l]
-                                      (let [[x] (keep-indexed #(when (= "S" %2) %1) l)]
-                                        (when (some? x) [y x])))))
-       [ny nx start-pipe] (match (mapv #(get-in pipes %)
-                                       [[(dec sy) sx]
-                                        [sy (inc sx)]
-                                        [(inc sy) sx]
-                                        [sy (dec sx)]])
-                            [(:or "|" "F" "7") _ (:or "|" "L" "J") _] [(dec sy) sx "|"]
-                            [_ (:or "-" "F" "L") _ (:or "-" "J" "7")] [sy (dec sx) "-"]
-                            [(:or "|" "F" "7") (:or "-" "F" "L") _ _] [(dec sy) sx "L"]
-                            [_ (:or "-" "F" "L") (:or "|" "L" "J") _] [sy (inc sx) "F"]
-                            [_ _ (:or "|" "L" "J") (:or "-" "J" "7")] [sy (dec sx) "7"]
-                            [(:or "|" "F" "7") _ _ (:or "-" "J" "7")] [(dec sy) sx "J"])
+(let [pipes (->> (read-input)
+                 (mapv #(str/split % #"")))
+      [[sy sx]] (->> pipes
+                     (keep-indexed (fn [y l]
+                                     (let [[x] (keep-indexed #(when (= "S" %2) %1) l)]
+                                       (when (some? x) [y x])))))
+      [ny nx start-pipe] (match (mapv #(get-in pipes %)
+                                      [[(dec sy) sx]
+                                       [sy (inc sx)]
+                                       [(inc sy) sx]
+                                       [sy (dec sx)]])
+                           [(:or "|" "F" "7") _ (:or "|" "L" "J") _] [(dec sy) sx "|"]
+                           [_ (:or "-" "F" "L") _ (:or "-" "J" "7")] [sy (dec sx) "-"]
+                           [(:or "|" "F" "7") (:or "-" "F" "L") _ _] [(dec sy) sx "L"]
+                           [_ (:or "-" "F" "L") (:or "|" "L" "J") _] [sy (inc sx) "F"]
+                           [_ _ (:or "|" "L" "J") (:or "-" "J" "7")] [sy (dec sx) "7"]
+                           [(:or "|" "F" "7") _ _ (:or "-" "J" "7")] [(dec sy) sx "J"])
 
-       pipes (assoc-in pipes [sy sx] start-pipe)
-       path (get-path pipes [ny nx] [sy sx])
-       path-set (set path)
-       outside-path (->> (set (combo/cartesian-product
-                               (range (count pipes))
-                               (range (count (first pipes)))))
-                         (filter #(not (contains? path-set %))))]
+      pipes (assoc-in pipes [sy sx] start-pipe)
+      path (get-path pipes [ny nx] [sy sx])
+      path-set (set path)
+      outside-path (->> (set (combo/cartesian-product
+                              (range (count pipes))
+                              (range (count (first pipes)))))
+                        (filter #(not (contains? path-set %))))]
 
-   (println "Part 1:" (/ (count path) 2))
+  (println "Part 1:" (/ (count path) 2))
 
-   (->> outside-path
-        (filter (partial enclosed? pipes path))
-        count
-        (println "Part 2:"))))
+  (->> outside-path
+       (filter (partial enclosed? pipes path))
+       count
+       (println "Part 2:")))
