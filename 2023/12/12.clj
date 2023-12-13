7c5c8f36ffcc758c5f23a80d6821151f30e1a6b5
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-13 12:39:38 +0100

    yolo

--- 2023/12/12.clj
+++ 2023/12/12.clj
@@ -8,21 +8,22 @@
   (memoize
    (fn [[springs groups]]
      (let [first-char (first springs)
-           group (first groups)]
+           group (first groups)
+           skip #(count-variants [(subs springs 1) groups])]
        (cond
          (empty? groups) (if (re-find #"#" springs) 0 1)
          (nil? first-char) 0
-         (= \. first-char) (recur [(subs springs 1) groups])
-         
+         (= \. first-char) (skip)
+
          (re-find (group-pattern group) springs)
-         (let [without-first-group (count-variants [(subs springs (min (inc group) (count springs))) (rest groups)])]
+         (let [use-first-group (count-variants [(subs springs (min (inc group) (count springs))) (rest groups)])]
            (if (= \# first-char)
-             without-first-group
-             (+ without-first-group
-                (count-variants [(subs springs 1) groups]))))
-         
+             use-first-group
+             (+ use-first-group
+                (skip))))
+
          (= \# first-char) 0
-         (= \? first-char) (recur [(subs springs 1) groups]))))))
+         (= \? first-char) (skip))))))
 
 (let [springs (->> (read-input)
                    (map #(str/split % #" "))
