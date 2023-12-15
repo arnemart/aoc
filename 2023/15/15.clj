280a88ea8127878cac4bc8f57c4362015a3a062a
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2023-12-15 11:42:13 +0100

    yolo

---
+++ 2023/15/15.clj
@@ -0,0 +1,29 @@
+(ns aoc.2023.15.15
+  (:require [aoc.common :refer [read-input]]
+            [flatland.ordered.map :refer [ordered-map]]))
+
+(defn HASH [s]
+  (reduce #(mod (* (+ %1 (int %2)) 17) 256) 0 s))
+
+(defn HASMAP [boxes step]
+  (let [[_ label op lens] (re-find #"(\w+)([=-])(\d+)?" step)
+        box (HASH label)]
+    (case op
+      "-" (update boxes box dissoc label)
+      "=" (update boxes box assoc label (parse-long lens)))))
+
+(let [steps (read-input :split-with #",")]
+
+  (->> steps
+       (map HASH)
+       (apply +)
+       (println "Part 1:"))
+
+  (->> steps
+       (reduce HASMAP (vec (repeat 256 (ordered-map))))
+       (map-indexed #(->> %2
+                          (map-indexed (fn [li [_ lens]]
+                                         (* (inc %1) (inc li) lens)))
+                          (apply +)))
+       (apply +)
+       (println "Part 2:")))
