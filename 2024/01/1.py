ce8c71f88c9ae47384a4f1158c06021c143f83b3
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2024-12-02 17:08:31 +0100

    yolo

--- 2024/01/1.py
+++ 2024/01/1.py
@@ -2,4 +2,4 @@ l=[int(x)for x in open("a").read().split()]
 s=sorted
 a=s(l[::2])
 b=s(l[1::2])
-print(sum((abs(x-y))for x,y in zip(a,b)),sum(x*b.count(x)for x in a))
+print(sum(abs(x-y) for x,y in zip(a,b)),sum(x*b.count(x)for x in a))
