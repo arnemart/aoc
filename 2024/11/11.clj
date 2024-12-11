49b3160c258a5311b707d08ed5932ca87a40bab5
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2024-12-11 14:00:26 +0100

    yolo

--- 2024/11/11.clj
+++ 2024/11/11.clj
@@ -1,7 +1,5 @@
 (ns aoc.2024.11.11
-  (:require
-   [aoc.common :refer [nums parse-input]]
-   [clojure.math :as math]))
+  (:require [aoc.common :refer [nums parse-input]]))
 
 (def blink
   (memoize
@@ -9,7 +7,7 @@
      (let [bl (partial blink (dec times))]
        (cond (zero? times) 1
              (zero? stone) (bl 1)
-             (odd? (long (math/log10 stone)))
+             (even? (count (str stone)))
              (let [s (str stone) h (/ (count s) 2)]
                (+ (bl (parse-long (subs s 0 h))) (bl (parse-long (subs s h)))))
              :else (bl (* stone 2024)))))))
