37aae9426941601d95ff6e48eac0f52cfc97f4e9
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2024-12-16 13:31:17 +0100

    yolo

--- 2024/16/16.clj
+++ 2024/16/16.clj
@@ -1,47 +1,48 @@
-(ns aoc.2024.16.16 
+(ns aoc.2024.16.16
   (:require
    [aoc.astar :refer [astar]]
-   [aoc.common :refer [lines manhattan parse-input spy]]
+   [aoc.common :refer [lines manhattan parse-input]]
    [blancas.kern.core :refer [many one-of*]]
    [clojure.math.combinatorics :refer [cartesian-product]]))
 
-(def t "#################
-#...#...#...#..E#
-#.#.#.#.#.#.#.#.#
-#.#.#.#...#...#.#
-#.#.#.#.###.#.#.#
-#...#.#.#.....#.#
-#.#.#.#.#.#####.#
-#.#...#.#.#.....#
-#.#.#####.#.###.#
-#.#.#.......#...#
-#.#.###.#####.###
-#.#.#...#.....#.#
-#.#.#.#####.###.#
-#.#.#.........#.#
-#.#.#.#########.#
-#S#.............#
-#################")
+(defn get-path [maze start end is-end]
+  (astar :start start
+         :is-end is-end
+         :get-neighbors (fn [[y x]]
+                          (->> [[(inc y) x \v] [(dec y) x \^] [y (inc x) \>] [y (dec x) \<]]
+                               (filter (fn [[y x]] (contains? #{\. \E} (get-in maze [y x]))))))
+         :calculate-cost (fn [[_ _ d1] [_ _ d2]]
+                           (if (= d1 d2) 1 1001))
+         :heuristic (fn [[x y]] (manhattan [y x] end))))
 
-(defn find-neighbors [maze [y x]]
-  (->> [[(inc y) x \v] [(dec y) x \^] [y (inc x) \>] [y (dec x) \<]]
-       (filter (fn [[y x]] (contains? #{\. \E} (get-in maze [y x]))))))
+(let [maze (parse-input (lines (many (one-of* "#.SE"))))
+      [[sy sx] end] (->> (cartesian-product (range (count maze)) (range (count (first maze))))
+                         (reduce (fn [[s e] p]
+                                   (if (and s e) (reduced [s e])
+                                       (case (get-in maze p)
+                                         \S [p e]
+                                         \E [s p]
+                                         [s e])))
+                                 [nil nil]))
 
-(let [maze (parse-input (lines (many (one-of* "#.SE"))) :test t)
-      [start end] (->> (cartesian-product (range (count maze)) (range (count (first maze))))
-                       (reduce (fn [[s e] p]
-                                 (if (and s e) (reduced [s e])
-                                     (let [v (get-in maze p)]
-                                       (cond (= \S v) [p e]
-                                             (= \E v) [s p]
-                                             :else [s e]))))
-                               [nil nil]))
-      best-path (astar :start (concat start [\>])
-                       :is-end #(= end (drop-last %))
-                       :get-neighbors (partial find-neighbors maze)
-                       :calculate-cost (fn [[_ _ d1] [_ _ d2]]
-                                         (if (= d1 d2) 1 1001))
-                       :heuristic (fn [[x y]] (manhattan [y x] end)))]
+      shortest-path (:cost (get-path maze [sy sx \>] end #(= end (drop-last %))))]
 
-  (println "Part 1:" (:cost best-path))
-  (count (:path best-path)))
+  (println "Part 1:" shortest-path)
+
+  (->> (cartesian-product (range (count maze)) (range (count (first maze))))
+       (filter #(= \. (get-in maze %)))
+       (filter (fn [[y x]]
+                 (->> [\> \^ \< \v]
+                      (some (fn [d]
+                              (let [from-start (get-path maze [sy sx \>] [y x] #(= [y x d] %))
+                                    h (manhattan [y x] end)]
+                                (if (and (some? from-start)
+                                         (<= (+ h (:cost from-start)) shortest-path))
+                                  (let [to-end (get-path maze (first (:path from-start)) end #(= end (drop-last %)))]
+                                    (if to-end
+                                      (= shortest-path (+ (:cost from-start) (:cost to-end)))
+                                      false))
+                                  false)))))))
+       count
+       (+ 2)
+       (println "Part 2:")))
