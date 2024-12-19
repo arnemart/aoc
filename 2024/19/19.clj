41d8f3f7bd8e89cd8f12936a33a0819f7f6a1ab3
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2024-12-19 20:21:15 +0100

    yolo

--- 2024/19/19.clj
+++ 2024/19/19.clj
@@ -4,14 +4,6 @@
    [blancas.kern.core :refer [<$> <*> >> new-line*]]
    [clojure.string :as str]))
 
-(defn possible [towels design]
-  (if (= "" design)
-    true
-    (->> towels
-         (keep #(when (str/starts-with? design %)
-                  (subs design (count %))))
-         (some (partial possible towels)))))
-
 (def count-combinations
   (memoize
    (fn [towels design]
@@ -26,12 +18,9 @@
 (let [[towels designs] (parse-input (<*> (<$> set (comma-or-space-sep any-word))
                                          (>> new-line* new-line*
                                              (lines any-word))))
-      elemental-towels (filter #(not (possible (disj towels %) %)) towels)
-      possible-designs (filter (partial possible elemental-towels) designs)]
+      combinations (->> designs
+                        (map (partial count-combinations towels))
+                        (filter #(> % 0)))]
 
-  (println "Part 1:" (count possible-designs))
-
-  (->> possible-designs
-       (map (partial count-combinations towels))
-       (apply +)
-       (println "Part 2:")))
+  (println "Part 1:" (count combinations))
+  (println "Part 2:" (apply + combinations)))
