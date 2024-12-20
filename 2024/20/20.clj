3b0fedfb77729083b89abdd083d1ab5544230283
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2024-12-20 15:39:43 +0100

    yolo

--- 2024/20/20.clj
+++ 2024/20/20.clj
@@ -22,31 +22,31 @@
          (fn [[y x]] (->> [[(inc y) x] [(dec y) x] [y (inc x)] [y (dec x)]]
                           (filter #(contains? #{\. \E} (get-in maze %)))))))
 
-(defn cheats [maze honest-path visited honest-cost deltas]
-  (->> (range 1 (count honest-path))
-       (mapcat (fn [i]
-                 (let [path (take i honest-path)
-                       current (last path)
-                       cost-here (get-in visited [current :g])]
-                   (->> deltas
-                        (map (fn [[p c]] [(++ p current) c]))
-                        (filter #(contains? #{\. \E} (get-in maze (first %))))
-                        (map (fn [[p c]] [(get-in visited [p :g] honest-cost) c]))
-                        (filter #(> (first %) cost-here))
-                        (map #(+ (last %) (- honest-cost (- (first %) cost-here))))))))
-       (map #(- honest-cost %))
-       (filter #(>= % 100))
-       count))
+(defn cheats [maze {visited :visited honest-cost :cost path :path} deltas]
+  (let [honest-path (reverse path)]
+    (->> (range 1 (count honest-path))
+         (mapcat (fn [i]
+                   (let [path (take i honest-path)
+                         current (last path)
+                         cost-here (get-in visited [current :g])]
+                     (->> deltas
+                          (map (fn [[p c]] [(++ p current) c]))
+                          (filter #(contains? #{\. \E} (get-in maze (first %))))
+                          (map (fn [[p c]] [(get-in visited [p :g] honest-cost) c]))
+                          (filter #(> (first %) cost-here))
+                          (map #(+ (last %) (- honest-cost (- (first %) cost-here))))))))
+         (map #(- honest-cost %))
+         (filter #(>= % 100))
+         count)))
 
 (let [maze (parse-input (lines (many (one-of* ".#SE"))))
       [start end] (get-start-end maze)
-      {visited :visited honest-cost :cost path :path} (get-path start end maze)
-      honest-path (reverse path)]
+      honest-attempt (get-path start end maze)]
 
-  (println "Part 1:" (cheats maze honest-path visited honest-cost
+  (println "Part 1:" (cheats maze honest-attempt
                              [[[2 0] 2] [[-2 0] 2] [[0 2] 2] [[0 -2] 2]]))
 
-  (println "Part 2:" (cheats maze honest-path visited honest-cost
+  (println "Part 2:" (cheats maze honest-attempt
                              (->> (cartesian-product (range -20 21) (range -20 21))
                                   (map #(vector % (manhattan [0 0] %)))
                                   (filter #(<= 2 (last %) 20))))))
