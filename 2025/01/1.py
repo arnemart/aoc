a2cc4d602aa7f3db06a28232f5a3eaee76bc2080
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2025-12-01 15:05:37 +0100

    jul berre ein gong fer året

--- 2025/01/1.py
+++ 2025/01/1.py
@@ -1,6 +1,6 @@
-p=[(int(l[1:]),l[0])for l in open("a").read().split()]
-v=50;a=0;b=0
-for d,r in p:
- for i in range(d):v=(v+{"L":-1,"R":1}[r])%100;b+=v==0
+p=[l for l in open("a").read().split()]
+v=50;a=b=0
+for d in p:
+ for i in range(int(d[1:])):v=(v+{"L":-1,"R":1}[d[0]])%100;b+=v==0
  a+=v==0
 print(a,b)
