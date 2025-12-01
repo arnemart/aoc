7105a538778ddd71ee006d2cf0236a877b4b6bc9
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2025-12-01 11:44:28 +0100

    jul berre ein gong fer året

--- 2025/01/1.clj
+++ 2025/01/1.clj
@@ -1,27 +1,23 @@
 (ns aoc.2025.01.1
   (:require
    [aoc.common :refer [lines parse-input]]
-   [blancas.kern.core :refer [<*> dec-num one-of*]]
-   [clojure.math :refer [floor]]))
+   [blancas.kern.core :refer [<*> dec-num one-of*]]))
 
 (let [turns (->> (parse-input (lines (<*> (one-of* "LR") dec-num)))
                  (map (fn [[dir dist]] (if (= \L dir) (- dist) dist))))
 
-      zeroes-1 (->> turns
-                    (reduce (fn [[prev zeroes] d]
-                              (let [next (mod (+ prev d) 100)]
-                                [next (if (= 0 next) (inc zeroes) zeroes)])) [50 0]) 
-                    last)
+      zeroes (->> turns
+                  (reduce (fn [[prev zeroes-1 zeroes-2] d]
+                            (let [next (+ prev d)
+                                  nextmod (mod next 100)]
+                              [nextmod
+                               (if (= 0 nextmod) (inc zeroes-1) zeroes-1)
+                               (+ zeroes-2
+                                  (if (= 0 next) 1
+                                      (+ (quot (abs next) 100)
+                                         (if (and (< next 0) (not= 0 prev)) 1 0))))]))
+                          [50 0 0])
+                  (drop 1))]
 
-      zeroes-2 (->> turns
-                    (reduce (fn [[prev zeroes] d]
-                              (let [next (+ prev d)]
-                                [(mod next 100)
-                                 (+ zeroes
-                                    (if (= 0 next) 1 (+ (int (floor (/ (abs next) 100)))
-                                                        (if (and (< next 0) (not= 0 prev)) 1 0))))]))
-                            [50 0])
-                    last)]
-
-  (println "Part 1:" zeroes-1)
-  (println "Part 2:" zeroes-2))
+  (println "Part 1:" (first zeroes))
+  (println "Part 2:" (last zeroes)))
