54a5e9597fa79774de3de544c25c0aa652a9acd4
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2025-12-03 15:37:01 +0100

    jul berre ein gong fer året

--- 2025/03/3.py
+++ 2025/03/3.py
@@ -1,5 +1,5 @@
 def f(l,n):
- a=max(l[:len(l)-n+1])
+ a=max(l[:len(l)-n])
  while l[0]!=a:l=l[1:]
- return a if n<2 else a+f(l[1:],n-1)
-print([sum(int(f(l.strip(),n))for l in open("a"))for n in[2,12]])
+ return a if n<1 else a+f(l[1:],n-1)
+[print(sum(int(f(l.strip(),n))for l in open("a")))for n in[1,11]]
