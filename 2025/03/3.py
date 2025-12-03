3b9ead17c85cc29a810861ba02a13c0793ff47b8
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2025-12-03 15:11:12 +0100

    jul berre ein gong fer året

--- 2025/03/3.py
+++ 2025/03/3.py
@@ -1,3 +1,5 @@
-x=lambda l,i=0,m=(0,0):m if l==[]else x(l[1:],i+1,(i,l[0])if l[0]>m[1]else m)
-def s(l,n):(a,b)=x(l[:len(l)-n]);return str(b)+s(l[a+1:],n-1)if n>=0 else""
-print([sum(int(s(a,d))for a in[[int(c)for c in l[:-1]]for l in open("a")])for d in[1,11]])
+def f(l,n):
+ a=max(l[:len(l)-n+1])
+ while l[0]!=a:l=l[1:]
+ return a if n<2 else a+f(l[1:],n-1)
+print([sum(int(f(l.strip(),n))for l in open("a"))for n in[2,12]])
