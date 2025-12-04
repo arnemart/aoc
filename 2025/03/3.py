3c257a75069768c52327ce8c96e0e4b7e97f976a
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2025-12-04 11:02:34 +0100

    jul berre ein gong fer året

--- 2025/03/3.py
+++ 2025/03/3.py
@@ -1,2 +1,2 @@
 def f(l,n):a=max(l[:-n]);return a if n<2 else a+f(l.split(a,1)[1],n-1)
-[print(sum(int(f(l,n))for l in open("a")))for n in[2,12]]
+[print(sum(int(f(l.strip()+" ",n))for l in open("a")))for n in[2,12]]
