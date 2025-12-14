c2e92972396ad79d3b072bb588c903d5d6c22938
Arne Martin Aurlien <arne.martin.aurlien@fiken.no>
2025-12-14 13:03:36 +0100

    jul berre ein gong fer året

--- 2025/12/12.py
+++ 2025/12/12.py
@@ -1,1 +1,1 @@
-print(len([l for l in open("a")if"x"in l and int(l[:2])*int(l[3:5])>=sum(map(int,l[7:].split(" ")))*9]))
+print(sum(eval(l[:2]+"/8*"+l[3:5])>sum(map(int,l[7:].split()))for l in open("a")if"x"in l))
